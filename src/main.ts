031cf24f1b548d4b85350be277a0e93ab493da2c
Ahmed Atef <43449355+ahmedatefdev@users.noreply.github.com>
2023-11-09 20:33:14 +0200

    update otigin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://easygenerator-auth-fe-app.vercel.app',
+    ],
+    credentials: true,
+  });
 
   app.use(
     session({
